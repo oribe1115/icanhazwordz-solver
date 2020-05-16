ccb3e46ebe8cffab5b0f1c77072f14ffcf6521ac
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-05-17 01:13:31 +0900

    ファイル整理

--- solver/manualSolver.go
+++ solver/manualSolver.go
@@ -3,8 +3,6 @@ package solver
 import (
 	"fmt"
 
-	"github.com/siddontang/go/log"
-
 	"github.com/oribe1115/icanhazwordz-solver/lib"
 )
 
@@ -17,41 +15,4 @@ func SolverWithHand(dictionary lib.WordList) {
 	}
 
 	fmt.Println("finish")
-}
-
-func solver(dictionary lib.WordList, target string) (string, int) {
-	sortedTarget := lib.StringSort(target)
-	countQ, countU := lib.CountQU(sortedTarget)
-	add := ""
-	for i := 0; i < countQ; i++ {
-		add += "u"
-	}
-	sortedTarget = lib.StringSort(sortedTarget + add)
-
-	list := lib.WordList{}
-	for _, word := range dictionary {
-		if word.IsEnableConstruct(sortedTarget) {
-			list = append(list, word)
-		}
-	}
-
-	maxIndex := 0
-	maxScore := 0
-	for i, word := range list {
-		score := word.CalcScore(countQ, countU)
-		if score > maxScore || (score == maxScore && len(word.Sorted) > len(list[maxIndex].Sorted)) {
-			maxScore = score
-			maxIndex = i
-		}
-	}
-
-	log.Errorf("maxIndex: %d", maxIndex)
-	if list[maxIndex] == nil {
-		log.Errorf("nil index at taraget: %s", target)
-	}
-	if len(list[maxIndex].Examples) == 0 {
-		log.Errorf("no example at %d", maxIndex)
-	}
-
-	return list[maxIndex].Examples[0], maxScore
 }
--- solver/solver.go
+++ solver/solver.go
@@ -1,158 +1,43 @@
 package solver
 
 import (
-	"fmt"
-	"strconv"
-	"time"
-
 	"github.com/oribe1115/icanhazwordz-solver/lib"
-	"github.com/sclevine/agouti"
 	"github.com/siddontang/go/log"
 )
 
-var (
-	pageURL = "https://icanhazwordz.appspot.com/"
-)
-
-func AutoSolver(dictionary lib.WordList, sleepTime time.Duration, logMode bool) {
-	agoutiDriver := agouti.ChromeDriver()
-	agoutiDriver.Start()
-	defer agoutiDriver.Stop()
-	page, err := agoutiDriver.NewPage()
-	if err != nil {
-		log.Error(err)
-		return
+func solver(dictionary lib.WordList, target string) (string, int) {
+	sortedTarget := lib.StringSort(target)
+	countQ, countU := lib.CountQU(sortedTarget)
+	add := ""
+	for i := 0; i < countQ; i++ {
+		add += "u"
 	}
+	sortedTarget = lib.StringSort(sortedTarget + add)
 
-	page.Navigate(pageURL)
-	logBuffer := ""
-	totalScore := 0
-
-	for i := 0; i < 10; i++ {
-		logs, score, err := autoSolve(dictionary, page, sleepTime)
-		if err != nil {
-			log.Error(err)
-			return
-		}
-		totalScore += score
-		if logMode {
-			fmt.Printf("turn: %d\n%s", i+1, logs)
-		} else {
-			logBuffer += fmt.Sprintf("turn: %d\n%s", i+1, logs)
+	list := lib.WordList{}
+	for _, word := range dictionary {
+		if word.IsEnableConstruct(sortedTarget) {
+			list = append(list, word)
 		}
 	}
 
-	if !logMode {
-		fmt.Printf(logBuffer)
-	}
-
-	fmt.Printf("totalScore: %d\n", totalScore)
-
-	fmt.Printf("quit?\n> ")
-	input := lib.ReadLine()
-	if len(input) != 0 {
-		return
-	}
-}
-
-func AutoSolverToBeFirst(dictionary lib.WordList) {
-	fmt.Println("targetScore")
-	fmt.Print("> ")
-	targetScore, err := strconv.Atoi(lib.ReadLine())
-	if err != nil {
-		log.Error(err)
-		return
-	}
-
-	fmt.Println("limit")
-	fmt.Print("> ")
-	limit, err := strconv.Atoi(lib.ReadLine())
-	if err != nil {
-		log.Error(err)
-		return
-	}
-
-	agoutiDriver := agouti.ChromeDriver()
-	agoutiDriver.Start()
-	defer agoutiDriver.Stop()
-	page, err := agoutiDriver.NewPage()
-	if err != nil {
-		log.Error(err)
-		return
-	}
-
-	page.Navigate(pageURL)
-
-	for i := 0; i < limit; i++ {
-		for j := 0; j < 10; j++ {
-			_, _, err := autoSolve(dictionary, page, 0)
-			if err != nil {
-				log.Error(err)
-				return
-			}
+	maxIndex := 0
+	maxScore := 0
+	for i, word := range list {
+		score := word.CalcScore(countQ, countU)
+		if score > maxScore || (score == maxScore && len(word.Sorted) > len(list[maxIndex].Sorted)) {
+			maxScore = score
+			maxIndex = i
 		}
-
-		printedScore := page.FindByXPath("/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[1]/td[2]")
-		got, err := printedScore.Text()
-		if err != nil {
-			log.Error(err)
-		}
-
-		score, err := strconv.Atoi(got)
-		if err != nil {
-			log.Error(err)
-		} else {
-			fmt.Printf("%3d times: %4d\n", i, score)
-			if score > targetScore {
-				break
-			}
-		}
-
-		page.FindByLink("Start a new game").Click()
 	}
 
-	fmt.Printf("quit?\n> ")
-	input := lib.ReadLine()
-	if len(input) != 0 {
-		return
+	log.Errorf("maxIndex: %d", maxIndex)
+	if list[maxIndex] == nil {
+		log.Errorf("nil index at taraget: %s", target)
 	}
-}
-
-func autoSolve(dictionary lib.WordList, page *agouti.Page, sleepTime time.Duration) (string, int, error) {
-	err := page.Refresh()
-	if err != nil {
-		return "", 0, err
-	}
-	time.Sleep(sleepTime * time.Second)
-
-	letterClass := page.AllByClass("letter")
-	letterElement, err := letterClass.Elements()
-	if err != nil {
-		return "", 0, err
+	if len(list[maxIndex].Examples) == 0 {
+		log.Errorf("no example at %d", maxIndex)
 	}
 
-	target := ""
-
-	for _, l := range letterElement {
-		t, err := l.GetText()
-		if err != nil {
-			return "", 0, err
-		}
-
-		target += string(t[0])
-	}
-
-	answer, score := solver(dictionary, target)
-
-	inputField := page.FindByID("MoveField")
-	inputField.Fill(answer)
-	time.Sleep(sleepTime * time.Second)
-
-	submitButton := page.FindByButton("Submit")
-	err = submitButton.Click()
-	if err != nil {
-		return "", 0, err
-	}
-
-	return fmt.Sprintf(" %s\n%s: %d\n", target, answer, score), score, nil
+	return list[maxIndex].Examples[0], maxScore
 }

55ab20819ac70d1d2541d25c64f3f8cc4d7128c4
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-05-16 22:52:33 +0900

    fix

--- solver/simple-solver.go
+++ solver/simple-solver.go
@@ -8,7 +8,7 @@ import (
 
 func SolverWithHand(dictionary lib.WordList) {
 	for i := 0; i < 10; i++ {
-		fmt.Printf("turn: %d\n > ", i)
+		fmt.Printf("turn: %d\n > ", i+1)
 		target := lib.ReadLine()
 		answer, score := solver(dictionary, target)
 		fmt.Printf("%s: %d\n", answer, score)
@@ -37,7 +37,7 @@ func solver(dictionary lib.WordList, target string) (string, int) {
 	maxScore := 0
 	for i, word := range list {
 		score := word.CalcScore(countQ, countU)
-		if score > maxScore {
+		if score > maxScore || (score == maxScore && len(word.Sorted) > len(list[maxIndex].Sorted)) {
 			maxScore = score
 			maxIndex = i
 		}
